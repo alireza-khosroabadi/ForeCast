31bb75374ab6473bfe41ed721e85df4b853534fc
KhosrowabadiAlireza <khosroabadi.alireza@gmail.com>
2023-08-28 22:42:33 +0330

    remove comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,6 @@ kapt {
 
 dependencies {
 
-//    def fragment_version "1.7.0-alpha03"
-
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
